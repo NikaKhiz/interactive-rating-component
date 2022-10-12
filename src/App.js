5c96969fa0df6119cec4fb1e66f84b38642a2923
NikaKhiz <khizambarelinika123@gmail.com>
2022-10-12 22:43:09 +0400

    initil setup of react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h1>The repo is made for react bootcamp challenges</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
